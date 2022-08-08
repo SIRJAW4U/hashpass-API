515e303dc259077a8cc7b4029a61883c943842ce
Johnnywoode <johnnywoode@gmail.com>
2022-08-08 02:10:43 +0000

    login and logout functionality

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Hash;
 
 class AuthController extends Controller
 {
@@ -29,5 +30,34 @@ class AuthController extends Controller
       ];
 
       return response($response, Response::HTTP_CREATED);
+    }
+
+    public function signIn(Request $request){
+			$fields = $request->validate([
+					'email' => 'required|string',
+					'password' => 'required|string',
+			]);
+
+			$user = User::where('email', $fields['email'])->first();
+            if(!$user || !Hash::check($fields['password'], $user->password)){
+                return response([
+                    'message' => 'bad credentials'
+                ], Response::HTTP_UNAUTHORIZED);
+            }
+
+			$token = $user->createToken('apitoken')->plainTextToken;
+
+			$response = [
+					'user' => $user,
+					'token' =>$token
+			];
+
+			return response($response, Response::HTTP_CREATED);
+		}
+
+    public function signOut(Request $request){
+        auth()->user()->tokens()->delete();
+
+        return ['message' => 'Signed out'];
     }
 }
